a07698b56b6e71112553cf14e4d044c907108a68
enuggetry <github@meyao.net>
2016-03-31 18:54:32 -0700

    first commit

---
+++ plugins/GalaxyBlaster/js/main.js
@@ -0,0 +1,9 @@
+/* 
+ * JBrowse Galaxy Blaster - Client Side Plugin
+ */
+/* 
+    Created on : Mar 31, 2016, 6:38:33 PM
+    Author     : ey
+*/
+
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+# JBrowse Galaxy Blaster Plugin
