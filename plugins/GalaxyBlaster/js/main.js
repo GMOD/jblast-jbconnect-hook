46b8ebc7499b6a210822d53473ac54d399075b63
enuggetry <github@meyao.net>
2016-04-05 06:49:31 -0700

    Add Blast button

--- plugins/GalaxyBlaster/js/main.js
+++ plugins/GalaxyBlaster/js/main.js
@@ -1,9 +1,81 @@
 /* 
- * JBrowse Galaxy Blaster - Client Side Plugin
- */
-/* 
-    Created on : Mar 31, 2016, 6:38:33 PM
+    JBrowse Galaxy Blaster - Client Side Plugin
+
+    Created on : Mar 20, 2016, 6:38:33 PM
     Author     : ey
 */
 
+define([
+        'dojo/_base/declare',
+        'dojo/_base/lang',
+        'dojo/Deferred',
+        'dojo/dom-construct',
+        'dojo/query',
+        'JBrowse/Plugin'
+       ],
+       function(
+        declare,
+        lang,
+        Deferred,
+        domConstruct,
+        query,
+        JBrowsePlugin
+       ) {
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+        console.log("plugin: GalaxyBlaster");
+        //console.dir(args);
 
+        var thisB = this;
+        var browser = this.browser;
+
+        /*
+        dojo.subscribe("/jbrowse/v1/v/tracks/show", function(data){
+            console.log("GalaxyBlaster trapped /jbrowse/v1/v/tracks/show");
+            console.dir(data);
+        });
+        */
+        // trap the redraw event for handling resize
+        /*
+        dojo.subscribe("/jbrowse/v1/n/tracks/redraw", function(data){
+            console.dir(browser.view.tracks);
+            for (var i in browser.view.tracks) {
+                console.log(browser.view.tracks[i].labelHTML);
+                console.log(browser.view.tracks[i].config.menuTemplate);
+            }
+        });
+        */
+        // create function intercept after view initialization (because the view object doesn't exist before that)
+        browser.afterMilestone( 'loadConfig', function() {
+            
+            require(["dojo/_base/lang", "JBrowse/View/FASTA"], function(lang, FASTA){
+                /*
+                lang.extend(FASTA, {
+                    renderHTML: function( region, seq, parent ) {
+                        console.log("intercept renderHTML()");
+                        console.dir(this);
+                        //return this.old_renderHTML( region, seq, parent );
+                    }
+                    
+                });
+                */
+            });
+        });
+        // create function intercept after view initialization (because the view object doesn't exist before that)
+        browser.afterMilestone( 'loadConfig', function() {
+            if (typeof browser.config.classInterceptList === 'undefined') {
+                browser.config.classInterceptList = {};
+            }
+            
+            // override ProcessedTranscripts
+            browser.config.classInterceptList["ProcessedTranscript"] = [function(obj) {
+	    	console.log("intercepting ProcessedTranscript");
+                obj._getFeatureHeight = function() {
+                    return 11;
+                };
+            }];
+        });      
+    }
+});
+});
---
+++ src/JBrowse/View/FASTA.js
@@ -0,0 +1,132 @@
+define([
+           'dojo/_base/declare',
+           'dojo/dom-construct',
+
+           'dijit/Toolbar',
+           'dijit/form/Button',
+           'JBrowse/Util',
+           'JBrowse/has',
+           'JBrowse/InHookMixin',
+           'dojo/request/xhr',
+           'dijit/Dialog'
+       ],
+       function( declare, dom, Toolbar, Button, Util, has, InHook, xhr,Dialog) {
+
+return declare([InHook],
+{
+
+    constructor: function( args ) {
+        
+        //this.inceptInit();
+        
+        this.width       = args.width || 78;
+        this.htmlMaxRows = args.htmlMaxRows || 15;
+        this.track = args.track;
+        this.canSaveFiles = args.track &&  args.track._canSaveFiles && args.track._canSaveFiles();
+        this.dialog = null;
+    },
+    renderHTML: function( region, seq, parent ) {
+        var thisB = this;
+        var text = this.renderText( region, seq );
+        var lineCount = text.match( /\n/g ).length + 1;
+        var container = dom.create('div', { className: 'fastaView' }, parent );
+
+        if( this.canSaveFiles ) {
+            var toolbar = new Toolbar().placeAt( container );
+            var thisB = this;
+            toolbar.addChild( new Button({ 
+                iconClass: 'dijitIconFunction',
+                label: 'BLAST',
+                title: 'BLAST this feature',
+                disabled: ! has('save-generated-files'),
+                onClick: function() {
+                    thisB.blastDialog();
+                }
+            }));
+                              
+            toolbar.addChild( new Button(
+                                  { iconClass: 'dijitIconSave',
+                                    label: 'FASTA',
+                                    title: 'save as FASTA',
+                                    disabled: ! has('save-generated-files'),
+                                    onClick: function() {
+                                        thisB.track._fileDownload(
+                                            { format: 'FASTA',
+                                              filename: Util.assembleLocString(region)+'.fasta',
+                                              data: text
+                                            });
+                                    }
+                                  }));
+        }
+
+        var textArea = dom.create('textarea', {
+                        className: 'fasta',
+                        cols: this.width,
+                        rows: Math.min( lineCount, this.htmlMaxRows ),
+                        readonly: true
+                    }, container );
+        var c = 0;
+        textArea.innerHTML = text.replace(/\n/g, function() { return c++ ? '' : "\n"; });
+        return container;
+    },
+    blastDialog: function () {
+        var thisB = this;
+        if (!this.dialog) {
+
+            this.dialog = new Dialog({ title: 'Process BLAST' });
+            dojo.create('div', {
+                id: 'blast-box',
+                innerHTML: 'This will process a BLAST search against the selected database.<br/><button id="submit-btn" type="button">Submit</button> <button id="cancel-btn" type="button">Cancel</button>'
+            }, this.dialog.containerNode /* the content portion of the dialog you're creating */);
+            //var div = dojo.create('div', {}, this.dialog.containerNode);
+            this.confirmBtn = null;
+            
+            var submitBtn = new Button({
+                label: "Submit",
+                onClick: function(){
+
+                    var confirmBox = new Dialog({ title: 'Confirmation' });
+                    dojo.create('div', {
+                        id: 'confirm-btn',
+                        innerHTML: 'BLAST submitted...'
+                    }, confirmBox.containerNode /* the content portion of the dialog you're creating */);
+                    confirmBox.show();
+
+                    setTimeout(function(){
+                        confirmBox.destroyRecursive();
+                    }, 2000);
+
+                    thisB.destroyBlastDialog();
+                }
+            }, "submit-btn").startup();
+            var cancelBtn = new Button({
+                label: "Cancel",
+                onClick: function(){
+                    thisB.destroyBlastDialog();
+                }
+            }, "cancel-btn").startup();
+
+        }
+        if (this.dialog) this.dialog.show();
+    },
+    destroyBlastDialog: function () {
+        this.dialog.destroyRecursive();
+        delete this.dialog;
+        this.dialog = null;
+    }, 
+    renderText: function( region, seq ) {
+        return '>' + region.ref
+            + ' '+Util.assembleLocString(region)
+            + ( region.type ? ' class='+region.type : '' )
+            + ' length='+(region.end - region.start)
+            + "\n"
+            + this._wrap( seq, this.width );
+    },
+    _wrap: function( string, length ) {
+        length = length || this.width;
+        return string.replace( new RegExp('(.{'+length+'})','g'), "$1\n" );
+    }
+});
+});
+
+
