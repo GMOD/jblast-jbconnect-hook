2753aa742247c34e1e20934bd6db476ced12634d
enuggetry <github@meyao.net>
2016-04-19 22:17:25 -0700

    add JbJob and Test models and controllers

---
+++ jbserve/server/api/controllers/JbJobController.js
@@ -0,0 +1,10 @@
+/**
+ * JbJobController
+ *
+ * @description :: Server-side logic for managing jbjobs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+};
+
