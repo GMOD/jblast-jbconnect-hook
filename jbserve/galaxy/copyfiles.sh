8017aa51ab29114be1433358f7d15477dcb31023
enuggetry <github@meyao.net>
2016-05-18 18:37:12 -0700

    add readme and copy script copyfiles.sh move files from from dev to appropriate galaxy paths.

---
+++ jbserve/galaxy/copyfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -v config/tool_conf.xml /var/www/html/galaxy/config/ # 
+cp -rv tools/jb_blast2gff/ /var/www/html/galaxy/tools/ #
+cp -rv tools/jb_exportgff/ /var/www/html/galaxy/tools/ #
---
+++ jbserve/galaxy/readme.md
@@ -0,0 +1,1 @@
+# Galaxy plugins (tools)
