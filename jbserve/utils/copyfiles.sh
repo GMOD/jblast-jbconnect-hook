d50d835cf0b0f9ee82d326a8fc442c94c841ce85
enuggetry <github@meyao.net>
2016-06-08 19:49:46 -0700

    add blastxml2json stuff

---
+++ jbserve/utils/copyfiles.sh
@@ -0,0 +1,6 @@
+cp -v add-track-json.js /var/www/html/galaxy/tools/jb_exportgff #
+cp -v inMemTemplate.json /var/www/html/galaxy/tools/jb_exportgff #
+cp -rv node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
+
+cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_exportgff #
+cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
