b32c8c62de524bbf7c65046c2855cd72d1159883
enuggetry <github@meyao.net>
2016-06-14 02:12:13 -0700

    improvements to copy script and galaxy tools.

--- jbserve/utils/copyfiles.sh
+++ jbserve/utils/copyfiles.sh
@@ -2,5 +2,5 @@ cp -v add-track-json.js /var/www/html/galaxy/tools/jb_exportgff #
 cp -v inMemTemplate.json /var/www/html/galaxy/tools/jb_exportgff #
 cp -rv node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
 
-cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_exportgff #
+cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
