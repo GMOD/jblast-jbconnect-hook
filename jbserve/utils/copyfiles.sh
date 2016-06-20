2e5a656c5560b6fd74e886731f333ba3fa95e11e
enuggetry <github@meyao.net>
2016-06-20 07:30:47 -0700

    non-verbose copy modules

--- jbserve/utils/copyfiles.sh
+++ jbserve/utils/copyfiles.sh
@@ -6,17 +6,17 @@ cp -v ../galaxy/config/tool_conf.xml /var/www/html/galaxy/config/ #
 mkdir -p /var/www/html/galaxy/tools/jb_exportgff #
 cp -v jbexport.js /var/www/html/galaxy/tools/jb_exportgff #
 cp -v inMemTemplate.json /var/www/html/galaxy/tools/jb_exportgff #
-cp -rv node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
+cp -r node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
 cp -rv ../galaxy/tools/jb_exportgff/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_blastxml2json #
-cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
+cp -r node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -rv ../galaxy/tools/jb_blastxml2json/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_blaststart #
 cp -v jblaststart.js /var/www/html/galaxy/tools/jb_blaststart #
-cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blaststart #
+cp -r node_modules/ /var/www/html/galaxy/tools/jb_blaststart #
 cp -rv ../galaxy/tools/jb_blaststart/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_blast2gff #
