b697585300bcdf6154a6a23d6a2a765e7ae51b62
enuggetry <github@meyao.net>
2016-06-19 23:41:55 -0700

    moved all copy operations into this.  galaxy/copyfiles.sh now obsoleted.

--- jbserve/utils/copyfiles.sh
+++ jbserve/utils/copyfiles.sh
@@ -1,9 +1,23 @@
+#!/bin/bash
+# copies all necessary files to setup galaxy tools
+# assumption: galaxy install dir is /var/www/html/galaxy
+cp -v ../galaxy/config/tool_conf.xml /var/www/html/galaxy/config/ # 
+
+mkdir -p /var/www/html/galaxy/tools/jb_exportgff #
 cp -v add-track-json.js /var/www/html/galaxy/tools/jb_exportgff #
 cp -v inMemTemplate.json /var/www/html/galaxy/tools/jb_exportgff #
 cp -rv node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
+cp -rv ../galaxy/tools/jb_exportgff/ /var/www/html/galaxy/tools/ #
 
+mkdir -p /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
+cp -rv ../galaxy/tools/jb_blastxml2json/ /var/www/html/galaxy/tools/ #
 
+mkdir -p /var/www/html/galaxy/tools/jb_blaststart #
 cp -v jblaststart.js /var/www/html/galaxy/tools/jb_blaststart #
 cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blaststart #
+cp -rv ../galaxy/tools/jb_blaststart/ /var/www/html/galaxy/tools/ #
+
+mkdir -p /var/www/html/galaxy/tools/jb_blast2gff #
+cp -rv ../galaxy/tools/jb_blast2gff/ /var/www/html/galaxy/tools/ #
