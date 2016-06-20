7b8524e1f8b5b8366a3231cdfd5c03d0b8bf1aec
enuggetry <github@meyao.net>
2016-06-19 17:49:10 -0700

    fix jb_blaststart directory

--- jbserve/utils/copyfiles.sh
+++ jbserve/utils/copyfiles.sh
@@ -5,5 +5,5 @@ cp -rv node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
 cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
 
-cp -v jblaststart.js /var/www/html/galaxy/tools/jb_jblaststart #
-cp -rv node_modules/ /var/www/html/galaxy/tools/jb_jblaststart #
+cp -v jblaststart.js /var/www/html/galaxy/tools/jb_blaststart #
+cp -rv node_modules/ /var/www/html/galaxy/tools/jb_blaststart #
