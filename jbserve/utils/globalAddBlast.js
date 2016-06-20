360b6c4de487316643705d062fe4db8be253f0fe
enuggetry <github@meyao.net>
2016-06-19 23:48:07 -0700

    adds temp blast setup to globals

---
+++ jbserve/utils/globalAddBlast.js
@@ -0,0 +1,38 @@
+/*
+ * add blast globals to global file 
+ */
+var fs = require("fs");
+
+var globalPath = "/etc/jbrowse";
+var globalFile = globalPath + "/globals.dat";
+
+var blastData = {
+            "name": "JBlast Test", 
+            "blastSeq": "/var/www/html/jb-galaxy-blaster/tmp/44705works.fasta",
+            "originalSeq": "/var/www/html/jb-galaxy-blaster/tmp/volvox.fa",
+            "offset": 44705
+    };
+
+storeInGlobals(blastData,"jblast");
+// add a new section to global data
+function storeInGlobals (sectionData,sectionName) {
+    
+    fs.readFile(globalFile, function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        var g = JSON.parse(data);
+        g[sectionName] = sectionData;
+
+        var gStr = JSON.stringify(g,null,4);
+
+        fs.writeFile(globalFile,gStr, function (err) {
+            if (err) throw err;
+            console.log("Global file: "+ globalFile);
+        });
+    });    
+        
+}
+
+
+
