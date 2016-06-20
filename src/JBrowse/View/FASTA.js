20b2d52a8f474af27a074f639ed570d7a661335a
enuggetry <github@meyao.net>
2016-06-20 07:31:18 -0700

    Add workflow selection to blast submit.

--- src/JBrowse/View/FASTA.js
+++ src/JBrowse/View/FASTA.js
@@ -8,9 +8,13 @@ define([
            'JBrowse/has',
            'JBrowse/InHookMixin',
            'dojo/request/xhr',
-           'dijit/Dialog'
+           'dijit/Dialog',
+           "dojo/store/Memory",
+           "dijit/form/ComboBox",
        ],
-       function( declare, dom, Toolbar, Button, Util, has, InHook, xhr,Dialog) {
+       function( declare, dom, Toolbar, Button, Util, has, InHook, xhr,Dialog,
+            Memory, ComboBox
+               ) {
 
 return declare([InHook],
 {
@@ -24,6 +28,8 @@ return declare([InHook],
         this.track = args.track;
         this.canSaveFiles = args.track &&  args.track._canSaveFiles && args.track._canSaveFiles();
         this.dialog = null;
+        
+        this.initData(args);
     },
     renderHTML: function( region, seq, parent ) {
         var thisB = this;
@@ -34,15 +40,8 @@ return declare([InHook],
         if( this.canSaveFiles ) {
             var toolbar = new Toolbar().placeAt( container );
             var thisB = this;
-            toolbar.addChild( new Button({ 
-                iconClass: 'dijitIconFunction',
-                label: 'BLAST',
-                title: 'BLAST this feature',
-                disabled: ! has('save-generated-files'),
-                onClick: function() {
-                    thisB.blastDialog(text);
-                }
-            }));
+            
+            thisB.addButtons(region, seq, toolbar);
                               
             toolbar.addChild( new Button(
                                   { iconClass: 'dijitIconSave',
@@ -69,26 +68,102 @@ return declare([InHook],
         textArea.innerHTML = text.replace(/\n/g, function() { return c++ ? '' : "\n"; });
         return container;
     },
+    initData: function(args) {
+        var thisB = this;
+        this.workflows = [];
+        
+        var xhrArgs = {
+          url: jbServer + "/jbapi/getworkflows",
+          handleAs: "json",
+          preventCache: true,
+          load: function(data){
+                console.log("get workflows result", data);
+                thisB.workflows = data;
+                
+          },
+          error: function(error){
+          }
+        }
+
+        // Call the asynchronous xhrGet
+        var deferred = dojo.xhrGet(xhrArgs);
+        
+    },
+    // adds Blast button
+    addButtons: function (region,seq, toolbar) {
+        var text = this.renderText( region, seq );
+        thisB = this;
+        toolbar.addChild( new Button({ 
+            iconClass: 'dijitIconFunction',
+            label: 'BLAST',
+            title: 'BLAST this feature',
+            disabled: ! has('save-generated-files'),
+            onClick: function() {
+                thisB.blastDialog(text);
+            }
+        }));
+    },
+    // display blast dialog
     blastDialog: function (region) {
         var regionB = region;
         var thisB = this;
         if (!this.dialog) {
 
             this.dialog = new Dialog({ title: 'Process BLAST' });
+
+            console.log("building dialog");
+
+            dojo.create('span', {
+                innerHTML: 'Workflow '
+            }, this.dialog.containerNode);
+            dojo.create('button', {
+                id: 'blast-workflow-select'
+            }, this.dialog.containerNode);
+
+                var comboData = [];
+                var workflows = thisB.workflows;
+
+                for(var i in this.workflows) {
+                    console.log("workflow",workflows[i]);
+                    if (!workflows[i].deleted) {
+                        comboData.push({'name': workflows[i].name, 'id':workflows[i].id});
+                    }
+                }
+                console.log("comboData",comboData);
+
+                var stateStore = new Memory({
+                    data: comboData
+                });
+
+                var comboBox = new ComboBox({
+                    id: "workflow-combo",
+                    name: "workflow",
+                    value: comboData[0].name,
+                    store: stateStore,
+                    searchAttr: "name"
+                }, "blast-workflow-select").startup();            
+            
             dojo.create('div', {
                 id: 'blast-box',
+                style: {'margin-top': '20px'},
                 innerHTML: 'This will process a BLAST search against the selected database.<br/><button id="submit-btn" type="button">Submit</button> <button id="cancel-btn" type="button">Cancel</button>'
             }, this.dialog.containerNode /* the content portion of the dialog you're creating */);
             //var div = dojo.create('div', {}, this.dialog.containerNode);
             this.confirmBtn = null;
+            
+
             
             var submitBtn = new Button({
                 label: "Submit",
                 onClick: function(){
                     // do http post
                     var xhrArgs = {
-                      url: 'http://192.168.56.102:1337/jbapi/blastregion',
-                      postData: {region: regionB},
+                      //url: jbServer + '/jbapi/blastregion',
+                      url: jbServer + '/jbapi/workflowsubmit',
+                      postData: {
+                          region: regionB,
+                          workflow: 'f2db41e1fa331b3e'
+                      },
                       handleAs: "json",
                       load: function(data){
                         console.log("POST result");
