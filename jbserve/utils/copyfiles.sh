e196bded1638bdfd8517812b754a46762a21bf4d
enuggetry <github@meyao.net>
2016-08-25 20:29:32 -0700

    make executable & stop copying node_modules

--- jbserve/utils/copyfiles.sh
+++ jbserve/utils/copyfiles.sh
@@ -4,24 +4,24 @@
 cp -v ../galaxy/config/tool_conf.xml /var/www/html/galaxy/config/ # 
 
 mkdir -p /var/www/html/galaxy/tools/jb_exportgff #
-cp -v jbexport.js /var/www/html/galaxy/tools/jb_exportgff #
+#cp -v jbexport.js /var/www/html/galaxy/tools/jb_exportgff #
 cp -v inMemTemplate.json /var/www/html/galaxy/tools/jb_exportgff #
-cp -r node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
+#cp -r node_modules/ /var/www/html/galaxy/tools/jb_exportgff #
 cp -rv ../galaxy/tools/jb_exportgff/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_blastxml2json #
-cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_blastxml2json #
-cp -r node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
+#cp -v blastxml2json.js /var/www/html/galaxy/tools/jb_blastxml2json #
+#cp -r node_modules/ /var/www/html/galaxy/tools/jb_blastxml2json #
 cp -rv ../galaxy/tools/jb_blastxml2json/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_blaststart #
-cp -v jblaststart.js /var/www/html/galaxy/tools/jb_blaststart #
-cp -r node_modules/ /var/www/html/galaxy/tools/jb_blaststart #
+#cp -v jblaststart.js /var/www/html/galaxy/tools/jb_blaststart #
+#cp -r node_modules/ /var/www/html/galaxy/tools/jb_blaststart #
 cp -rv ../galaxy/tools/jb_blaststart/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_offset #
-cp -v jboffset.js /var/www/html/galaxy/tools/jb_offset #
-cp -r node_modules/ /var/www/html/galaxy/tools/jb_offset #
+#cp -v jboffset.js /var/www/html/galaxy/tools/jb_offset #
+#cp -r node_modules/ /var/www/html/galaxy/tools/jb_offset #
 cp -rv ../galaxy/tools/jb_offset/ /var/www/html/galaxy/tools/ #
 
 mkdir -p /var/www/html/galaxy/tools/jb_blast2gff #
