77af38c7cfa2f0b4ae6394950d0eb4f0b345d225
enuggetry <github@meyao.net>
2016-09-23 22:27:05 -0700

    add global test functions

--- jblast-galaxy-tools/bin/testglobal.js
+++ jblast-galaxy-tools/bin/testglobal.js
@@ -1,6 +1,36 @@
+#!/usr/bin/env node
 
 var global = require('./global-rest.js');
+var getopt = require('node-getopt');
 
-global.getGlobals (function(g) {
-    console.log(g);
-})
+var getopt = new getopt([
+    ['g' , 'getcfg'       , 'get config'],
+    ['s' , 'setcfg'       , 'set config'],
+    ['n' , 'name=NAME'       , 'set config'],
+    ['v' , 'value=VALUE'       , 'set config'],
+    
+    ['h' , 'help'            , 'display this help']
+]);              // create Getopt instance
+getopt.bindHelp()     // bind option 'help' to default action
+opt = getopt.parseSystem(); // parse command line
+
+if (!process.argv.slice(2).length) {
+    getopt.showHelp();
+    process.exit(1);
+}
+
+var getcfg = opt.options['getcfg'];
+var setcfg = opt.options['setcfg'];
+var name = opt.options['name'];
+var val = opt.options['value'];
+
+if (setcfg) {
+    global.setConfig(name,val);
+}
+
+if (getcfg) {
+    if (typeof name === 'undefined')
+        console.log('returned',global.getConfig(''));
+    else
+        console.log('returned',global.getConfig(name));
+}
