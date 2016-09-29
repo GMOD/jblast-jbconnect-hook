5b07463da588ecd196a7953842c86d8bdaa9ef9e
enuggetry <github@meyao.net>
2016-09-29 00:41:30 -0700

    rename to jblast-tools from jblast-galaxy-tools

---
+++ jblast-tools/globals.js
@@ -0,0 +1,13 @@
+/*
+ * jblast globals
+ */
+module.exports = {
+    galaxy: {
+        galaxyUrl: "http://localhost:8080",
+        galaxyPath: "/var/www/html/galaxy_jblast",
+        galaxyAPIKey: "2bb67717b99a37e92e59003f93625c9b"
+    },
+    jblast: {
+        blastResultPath: "jblastdata"
+    }
+};
