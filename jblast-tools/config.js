f07fb8db29d7c4a5f612a129a5739a2e9d2800d9
enuggetry <github@meyao.net>
2016-09-29 14:58:32 -0700

    rename

---
+++ jblast-tools/config.js
@@ -0,0 +1,18 @@
+/*
+ * jblast globals
+ */
+module.exports = {
+    dataSet: [
+        {
+            dataPath: "sample_data/json/volvox/"
+        }
+    ],
+    galaxy: {
+        galaxyUrl: "http://localhost:8080",
+        galaxyPath: "/var/www/html/galaxy_jblast",
+        galaxyAPIKey: "3ac9578a0158a218b5f129c912795239"
+    },
+    jblast: {
+        blastResultPath: "jblastdata"
+    }
+};
