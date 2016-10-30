22348e5a32df9b15268087f8d4e945015110a459
enuggetry <github@meyao.net>
2016-10-30 14:01:20 -0700

    cleanup

--- plugins/JBlast/js/main.js
+++ plugins/JBlast/js/main.js
@@ -76,10 +76,7 @@ return declare( JBrowsePlugin,
             // override FASTA
             require(["dojo/_base/lang", "JBrowse/View/FASTA"], function(lang, FASTA){
                 lang.extend(FASTA, {
-                    //initData: thisB.FASTA_initData,
                     addButtons: thisB.FASTA_addButtons
-                    //blastDialog: thisB.FASTA_blastDialog,
-                    //destroyBlastDialog:thisB.FASTA_destroyBlastDialog
                 });
             });
             // override Browser
@@ -844,28 +841,6 @@ return declare( JBrowsePlugin,
             });
         },this);
     },
-    FASTA_initData: function(args) {
-        var thisB = this;
-        this.workflows = [];
-        this.dialog = null;
-        
-        var xhrArgs = {
-          url: jbServer + "/jbapi/getworkflows",
-          handleAs: "json",
-          preventCache: true,
-          load: function(data){
-                console.log("get workflows result", data);
-                thisB.workflows = data;
-                
-          },
-          error: function(error){
-          }
-        }
-
-        // Call the asynchronous xhrGet
-        var deferred = dojo.xhrGet(xhrArgs);
-        
-    },
     // adds Blast button
     FASTA_addButtons: function (region,seq, toolbar) {
         var text = this.renderText( region, seq );
@@ -880,122 +855,6 @@ return declare( JBrowsePlugin,
                 JBrowse.jblastDialog(text);
             }
         }));
-    },
-    // display blast dialog
-    FASTA_blastDialog: function (region) {
-        var regionB = region;
-        var thisB = this;
-        var comboData = [];
-        
-        if (!this.dialog) {
-
-            this.dialog = new Dialog({ title: 'Process BLAST' });
-
-            console.log("building dialog");
-
-            dojo.create('span', {
-                innerHTML: 'Workflow '
-            }, this.dialog.containerNode);
-            dojo.create('button', {
-                id: 'blast-workflow-select'
-            }, this.dialog.containerNode);
-
-                var workflows = thisB.workflows;
-
-                for(var i in this.workflows) {
-                    console.log("workflow",workflows[i]);
-                    if (!workflows[i].deleted) {
-                        comboData.push({'name': workflows[i].name, 'id':workflows[i].id});
-                    }
-                }
-                console.log("comboData",comboData);
-
-                var stateStore = new Memory({
-                    data: comboData
-                });
-
-                var comboBox = new ComboBox({
-                    id: "workflow-combo",
-                    name: "workflow",
-                    value: comboData[0].name,
-                    store: stateStore,
-                    searchAttr: "name"
-                }, "blast-workflow-select").startup();            
-            
-            dojo.create('div', {
-                id: 'blast-box',
-                style: {'margin-top': '20px'},
-                innerHTML: 'This will process a BLAST search against the selected database.<br/><button id="submit-btn" type="button">Submit</button> <button id="cancel-btn" type="button">Cancel</button>'
-            }, this.dialog.containerNode /* the content portion of the dialog you're creating */);
-            //var div = dojo.create('div', {}, this.dialog.containerNode);
-            this.confirmBtn = null;
-            
-
-            
-            var submitBtn = new Button({
-                label: "Submit",
-                onClick: function(){
-                    
-                    // get selected workflow
-                    var selStr = dijit.byId('workflow-combo').get('value');
-                    for(var x in comboData) {
-                        if (comboData[x].name == selStr) {
-                            var selWorkflow = comboData[x].id;
-                            console.log('Selected workflow',selWorkflow,comboData[x].name);
-                        }
-                    }
-                    console.log('Selected workflow',selWorkflow);
-                    
-                    // do http post
-                    var xhrArgs = {
-                      //url: jbServer + '/jbapi/blastregion',
-                      url: jbServer + '/jbapi/workflowsubmit',
-                      postData: {
-                          region: regionB,
-                          //workflow: 'f2db41e1fa331b3e'
-                          workflow: selWorkflow
-                      },
-                      handleAs: "json",
-                      load: function(data){
-                        console.log("POST result");
-                        console.dir(data);
-                      },
-                      error: function(error){
-                          alert(error);
-                      }
-                    };
-                    var deferred = dojo.xhrPost(xhrArgs);
-
-
-                    // show confirm submit box
-                    var confirmBox = new Dialog({ title: 'Confirmation' });
-                    dojo.create('div', {
-                        id: 'confirm-btn',
-                        innerHTML: 'BLAST submitted...'
-                    }, confirmBox.containerNode /* the content portion of the dialog you're creating */);
-                    confirmBox.show();
-
-                    setTimeout(function(){
-                        confirmBox.destroyRecursive();
-                    }, 2000);
-
-                    thisB.destroyBlastDialog();
-                }
-            }, "submit-btn").startup();
-            var cancelBtn = new Button({
-                label: "Cancel",
-                onClick: function(){
-                    thisB.destroyBlastDialog();
-                }
-            }, "cancel-btn").startup();
-
-        }
-        if (this.dialog) this.dialog.show();
-    },
-    FASTA_destroyBlastDialog: function () {
-        this.dialog.destroyRecursive();
-        delete this.dialog;
-        this.dialog = null;
     },
     // display blast dialog
     Browser_jblastDialog: function (region) {
