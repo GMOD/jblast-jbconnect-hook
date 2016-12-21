09a6ee19f8f9f3b208a53c9df3be6bd68d44517a
enuggetry <github@meyao.net>
2016-12-21 03:03:09 -0800

    relocate socket.io.js, sails.io.js

--- plugins/JBClient/js/main.js
+++ plugins/JBClient/js/main.js
@@ -5,10 +5,10 @@ define([
         'dojo/dom-construct',
         'dojo/query',
         'JBrowse/Plugin',
-// sails.io.js from node_modules/sails.io.js
-// socket.io.js from node_modules/socket.io-client
-        '/socket.io.js',
-        '/sails.io.js'
+// sails.io.js from jbserver/node_modules/sails.io.js
+// socket.io.js from jbserver/node_modules/socket.io-client
+        '/js/socket.io.js',
+        '/js/sails.io.js'
     ],
        function(
         declare,
