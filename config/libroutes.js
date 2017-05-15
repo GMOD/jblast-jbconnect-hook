10bb763e606c21bc795b58cc74593e15f38fc0a8
enuggetry <github@meyao.net>
2017-05-14 17:05:23 -0700

    setup use of client-side dependency libraries based on libroutes

--- config/libroutes.js
+++ config/libroutes.js
@@ -4,6 +4,7 @@
 module.exports = {
     lib: {
             'jquery.mb.extruder':       '/jblib/mb.extruder',
-            'jQuery-ui-Slider-Pips':    '/jblib/slider-pips'
+            'jQuery-ui-Slider-Pips':    '/jblib/slider-pips',
+            'jquery-ui-dist':           '/jblib/jquery-ui'
     }
 };
