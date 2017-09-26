55da552e3c38526e6d3f2ca59ba0c4c1d4afb800
enuggetry <github@meyao.net>
2017-09-26 00:52:40 -0700

    add WorkflowMgr model/controller

--- api/controllers/WorkflowMgrController.js
+++ api/controllers/WorkflowMgrController.js
@@ -1,17 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @module
+ * @description
+ * todo: document 
  */
 
 var jblastProc = require('../services/jblastProc');
+
 module.exports = {
     hello: function (req, res, next) {
         return res.send('WorkflowMgr hello');
     },
-    //getWorkflows: function (req, res, next) {
-    //    return res.send('test2 getWorkflows');
-    //}
     getWorkflows: jblastProc.getWorkflows
 };
 
