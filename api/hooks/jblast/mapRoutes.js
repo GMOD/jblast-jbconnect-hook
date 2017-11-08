c0676c4601883693b0743d85200ad27c40f0174c
enuggetry <github@meyao.net>
2017-11-08 00:46:59 -0800

    remove obsolete routes

--- api/hooks/jblast/mapRoutes.js
+++ api/hooks/jblast/mapRoutes.js
@@ -9,16 +9,8 @@ module.exports = {
     routes: function() {
         return {
             before: {
-                
             },
             after: {
-                'post /jbapi/workflowsubmit': jblastProc.workflowSubmit,
-                'get /jbapi/getworkflows': jblastProc.getWorkflows,
-                'post /jbapi/setfilter': jblastProc.setFilter,
-                'get /jbapi/getblastdata/:asset/:dataset': jblastProc.getBlastData,
-                'get /jbapi/gettrackdata/:asset/:dataset': jblastProc.getTrackData,
-                'get /jbapi/gethitdetails/:asset/:dataset/:hitkey': jblastProc.getHitDetails,
-                'get /jbapi/lookupaccession/:accession': jblastProc.lookupAccession
             }
             
         };
