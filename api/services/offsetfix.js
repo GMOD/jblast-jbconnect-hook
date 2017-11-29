76d82a839c60b9797624c5cca8e779eb15afdbf9
enuggetry <github@meyao.net>
2017-11-28 17:47:50 -0800

    save dataset under dataset.path

--- api/services/offsetfix.js
+++ api/services/offsetfix.js
@@ -15,7 +15,7 @@ module.exports = {
         
         sails.log.debug("offsetfix.process()");
         
-        var blastfile = g.jbrowsePath + kWorkflowJob.data.jbrowseDataPath + '/' + g.jblast.blastResultPath +'/'+ newTrackJson[0].label + ".json";
+        var blastfile = g.jbrowsePath + kWorkflowJob.data.dataset.path + '/' + g.jblast.blastResultPath +'/'+ newTrackJson[0].label + ".json";
         // attempt to read the file
         try {
             var content = fs.readFileSync(blastfile, 'utf8');
