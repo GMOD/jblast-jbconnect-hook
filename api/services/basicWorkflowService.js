b0e5616712e97e85ba6b4e0561b47dee2879b487
enuggetry <github@meyao.net>
2018-01-27 01:27:31 -0800

    add code for blastProfile handling issue #68

--- api/services/basicWorkflowService.js
+++ api/services/basicWorkflowService.js
@@ -9,6 +9,7 @@ var resolvePath = require('resolve-path');
 var fs = require("fs-extra");
 var util = require("./utils");
 var shelljs = require('shelljs'); 
+var appPath = require("app-root-path").path;
 
 module.exports = {
 
@@ -105,26 +106,30 @@ module.exports = {
         var theFullBlastFilePath = blastPath+'/'+theBlastFile; 
 
         console.log("blastPath",blastPath);
-        // if direcgtory doesn't exist, create it
-        if (!fs.existsSync(blastPath)){
-            fs.mkdirSync(blastPath);
-        }  
-
+        
+        try {
+            // if direcgtory doesn't exist, create it
+            fs.ensureDirSync(blastPath);
+        }
+        catch(err) {
+            sails.log.error(err);            
+            return cb(null,{status: 'error',err:err});
+        }
+        
         try {
             ws = fs.createWriteStream(theFullBlastFilePath);
             ws.write(region);
             ws.end();
         }
-        catch (e) {
-            sails.log.error(e,theFullBlastFilePath);
-            cb(null,{status: 'error', msg: "failed to write",err:e});
-            return;
+        catch (err) {
+            sails.log.error(err,theFullBlastFilePath);
+            return cb(null,{status: 'error', msg: "failed to write",err:err});
         }
 
         var blastData = {
-                name: "JBlast", 
-                blastSeq: theFullBlastFilePath,
-                offset: startCoord
+            name: "JBlast", 
+            blastSeq: theFullBlastFilePath,
+            offset: startCoord
         };
 
         //sails.log.debug('>>> jbrowse globals',g.jbrowse);
@@ -136,14 +141,69 @@ module.exports = {
         var name = workflow.split('.workflow.');
         
         
+        
         //kJob.data.requestParams = params;
         kJob.data.sequence = seq;
         kJob.data.blastData = blastData;
         kJob.data.seqFile = theFile;
 
+        this.determineBlastProfile(kJob);
+
         kJob.update(function() {});
         
         this.beginProcessing2(kJob);
+    },
+    /*
+     * creates kJob.data.blastOptions based on kJob.data.blastProfile
+     * also creates blastOptionFile
+     */
+    determineBlastProfile(kJob) {
+        let g = sails.config.globals.jbrowse;       
+        
+        if (typeof kJob.data.blastProfile === 'undefined') {
+            // use defaultBlastProfile
+            if (g.jblast.defaultBlastProfile) {
+                let profile = g.jblast.defaultBlastProfile;
+                if (typeof g.jblast.blastProfiles[profile] !== 'undefined') {
+                    kJob.data.blastOptions = g.jblast.blastProfiles[profile];
+                }
+            }
+        }
+        else {
+            if (typeof kJob.data.blastProfile === 'string') {
+                // get blast profile based on string
+                let profile = kJob.data.blastProfile;
+                if (typeof g.jblast.blastProfiles[profile] !== 'undefined') {
+                    kJob.data.blastOptions = g.jblast.blastProfiles[profile];
+                }
+                
+                // if not found use default
+            }
+            else if (typeof kJob.data.blastProfile === 'object') {
+                kJob.data.blastOptions = kJob.data.blastProfile;
+            }
+            else {
+                //type not supported.
+            }
+        }
+        // create blastOptionFile
+        if (typeof kJob.data.blastOptions !== "undefined") {
+            // create file blastOption file
+            var d = new Date();
+
+            // write blast option file
+            kJob.data.blastOptionFile = "/tmp/blast_option"+d.getTime()+".json";
+            let optionFile = appPath + kJob.data.blastOptionFile;
+            
+            try {
+                fs.writeFileSync(optionFile,JSON.stringify(kJob.data.blastOptions,null,4));
+            } catch (err) {
+                return kJob.kDoneFn(Error('faild to write blastOptionFile',optionFile));
+            }
+        }
+        else {
+            sails.log.error("blastProfile is not defiled");
+        }
     },
     beginProcessing2(kJob) {
         var g = sails.config.globals.jbrowse;
@@ -166,17 +226,27 @@ module.exports = {
         var outPath = g.jbrowsePath + kJob.data.dataset + '/' + g.jblast.blastResultPath;
 
         sails.log('>>> Executing workflow',wf);
+        
+        let optionFile = appPath + kJob.data.blastOptionFile;
 
-        var child = shelljs.exec(
-            'node '+ wf + ' --in '+kJob.data.blastData.blastSeq + ' --ext blastxml --out '+outPath,{async:true},
+        let cmd = 'node '+ wf + ' --in '+kJob.data.blastData.blastSeq + ' --ext blastxml --out '+outPath+' --profile '+optionFile;
+        sails.log.debug("cmd",cmd);
+
+        var child = shelljs.exec(cmd,{async:true},
             function(code, stdout, stderr) {
                 if (code !== 0) {    // completed in error
                     console.log('Script Exit code:', code, typeof code);
                     console.log('Script Program stderr:', stderr);                
                 }
-                // completed successfully
                 console.log('Script output [[[[', stdout,']]]]');
 
+                // if no workflowResults terminate
+                if (stdout.indexOf("workflowResults:") === -1) {
+                    sails.log.error("failed to find workflow results");
+                    return kJob.kDoneFn(Error('workflow failed -', stdout));
+                }
+                
+                // extract workflow results
                 var results = extractjson(stdout);
                 
                 // the script may generate any number of files so we rely on the script results to tell us the generated file
--- workflows/NCBI.blast.workflow.js
+++ workflows/NCBI.blast.workflow.js
@@ -2,6 +2,8 @@ var getopt = require('node-getopt');
 var fs = require("fs-extra");
 var shelljs = require("shelljs");
 var blast = require('blastjs');
+var appPath = require("app-root-path").path;
+var _ = require('lodash');
 
 var thisPath = process.cwd();
 var jbrowsePath = "./";
@@ -18,6 +20,8 @@ if (fs.pathExistsSync(thisPath+"/node_modules/jbrowse/utils")) {
 // command line options
 var getopt = new getopt([
     ['i' , 'in=NUMBER'          , 'input .fasta file'],
+//    ['d' , 'db=BLASTDB'         , 'reference blast database path or db installed in approot/blastdb']
+    ['p' , 'profile=FILE'       , 'blast profile (JSON file)'],
     ['e' , 'ext=<extension>'    , 'output extension'],
     ['o' , 'out=<path>'         , 'output path'],
     ['h' , 'help'               , 'display this help']
@@ -29,13 +33,48 @@ var  inFile     = opt.options['in'];
 var outFileExt  = opt.options['ext'];
 var outPath     = opt.options['out']
 
+var blastProfileFile = opt.options['profile'];
+
+if (!blastProfileFile) {
+    console.log("profile= option undefined");
+    process.exit(1);
+}
+let rawdata = "";
+let blastProfile = "";
+
+
+try {
+    rawdata = fs.readFileSync(blastProfileFile,'utf-8');  
+    blastProfile = JSON.parse(rawdata); 
+}
+catch (err) {
+    console.log(err);
+    process.exit(1);
+}
+console.log("blastProfile",blastProfile,blastProfileFile);
+
+if (!inFile) {
+    console.log("in= option undefined");
+    process.exit(1);
+}
+if (!outFileExt) {
+    console.log("ext= option undefined");
+    process.exit(1);
+}
+if (!outPath) {
+    console.log("out= option undefined");
+    process.exit(1);
+}
 
 blast.outputString(true); //optional, provides string output instead of JSON
 
-var dbPath = '/var/www/html/blastdb/htgs/13apr2014/htgs';
+//var dbPath = '/var/www/html/blastdb/htgs/13apr2014/htgs';
+//var dbPath = appPath+'/blastdb/htgs/htgs';
+
+
 
 var options = {
-        db: dbPath,
+//        db: dbPath,
         outfmt: 5,
         outputDirectory: outPath,   //appPath+'/tmp',
         queryFile: inFile,
@@ -43,7 +82,9 @@ var options = {
         returnParams: true          // workflow script should include this blast option
 };
 
-//console.log("blast options",options);
+_.extend(options, blastProfile);
+
+console.log("blast options",options);
 
 fs.ensureDir(options.outputDirectory);
 
@@ -60,7 +101,8 @@ blast.blast(options, function(err, results) {
         var ret = results;
         ret.err = err;
         console.log('workflowResults:',ret);
-        return err;
+        process.exit(1);
+        //return err;
     }
     console.log('workflowResults:',JSON.stringify(results,null,4));
     return;
