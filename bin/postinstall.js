4bb1b7f0406a1e9ef37fb27f19469b6895af43af
enuggetry <github@meyao.net>
2018-02-26 01:14:11 -0800

    Obsolete. The search html is now copied upon search execution.

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -55,5 +55,5 @@ async.each(dirList,
 // copy workflows/SearchProcess.html to jbrowse directory
 // currently, we assume jbrowse has already been installed as a module in the 
 // TODO: needs better handling (ie. in case JBrowse is installed elsewhere).
-console.log("copying SearchProcessing file...");
-shelljs.cp(modPath+'/workflows/SearchProcess.html',appPath+'/node_modules/jbrowse');
+//console.log("copying SearchProcessing file...");
+//shelljs.cp(modPath+'/workflows/SearchProcess.html',appPath+'/node_modules/jbrowse');
