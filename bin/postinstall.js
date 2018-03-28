8129b330482d527c4e90f304f18ffe52611748e2
enuggetry <github@meyao.net>
2018-03-27 17:17:48 -0700

    refactor dependency injection to use sails dependency injection framework.

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -4,7 +4,7 @@
 //shelljs.exec('node blast_getBlast.js');
 
 
-var appPath = require("app-root-path").path;
+var approot = require("app-root-path").path;
 var shelljs = require("shelljs");
 var modPath = ""+shelljs.pwd();
 var fs = require("fs-extra");
@@ -12,7 +12,7 @@ var async = require("async");
 
 
 console.log(">>> jblast postinstall");
-console.log("appPath",appPath,"modPath",modPath);
+console.log("appPath",approot,"modPath",modPath);
 
 // install jblast-simtools globally (access by galaxy simulation tool
 //process.chdir('node_modules/jbh-jblast/jblast-simtool');
@@ -20,8 +20,8 @@ console.log("appPath",appPath,"modPath",modPath);
 //process.chdir('../../..');
 
 
-//exit if modPath == appPath, don't need to copy anything.
-if (modPath === appPath)
+//exit if modPath == approot, don't need to copy anything.
+if (modPath === approot)
     process.exit(0);
     
 
@@ -32,7 +32,7 @@ var dirList = [
 
 async.each(dirList,
     function(item, cb){
-        var trg = appPath+"/"+item.src;
+        var trg = approot+"/"+item.src;
         var src = modPath+"/"+item.trg;
 
         console.log('copying ',src,trg);
@@ -62,4 +62,12 @@ async.each(dirList,
 // currently, we assume jbrowse has already been installed as a module in the 
 // TODO: needs better handling (ie. in case JBrowse is installed elsewhere).
 //console.log("copying SearchProcessing file...");
-//shelljs.cp(modPath+'/workflows/SearchProcess.html',appPath+'/node_modules/jbrowse');
+//shelljs.cp(modPath+'/workflows/SearchProcess.html',approot+'/node_modules/jbrowse');
+
+// copy plugin dependencies to assets/jblib
+console.log("Copying plugin dependencies to assets/jblib...");
+let targDir = approot+'/assets/jblib';
+fs.ensureDirSync(targDir);
+
+// slider-pips
+fs.copySync(approot+'/node_modules/jQuery-ui-Slider-Pips/dist',targDir,{overwrite:true});
