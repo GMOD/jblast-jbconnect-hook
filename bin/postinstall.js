fe49928832d6423857b83e6101afb8af5b3a2630
enuggetry <ericiam@berkeley.edu>
2018-09-04 16:58:07 -0400

    submited modules to npm repo for use

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -78,7 +78,7 @@ let package = require(approot+'/package.json');
 package.scripts['jblast-test'] = "mocha test/jblast-int/**/*.test.js test/bootstrap/bootstrap.test.js";
 fs.writeFileSync(approot+'/package.json', JSON.stringify(package,null,2));
 
-
+/*
 let cwd = shell.pwd();
 shell.cd(approot);
 
@@ -94,3 +94,4 @@ if (shell.exec('npm install enuggetry/faux-blastdb').code !== 0) {
 }
 
 shell.cd(cwd);  // restore working directory
+*/
