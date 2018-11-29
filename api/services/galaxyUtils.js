c566276f388f26bc44c4c416d61449d2b907cd46
enuggetry <ericiam@berkeley.edu>
2018-11-29 18:21:30 -0500

    move doCompleteAction - galaxy only

--- api/services/galaxyUtils.js
+++ api/services/galaxyUtils.js
@@ -448,11 +448,137 @@ module.exports = {
                     //kJob.update(function() {});
                     sails.log.debug(wId,'workflow completed');
                     setTimeout(function() {
-                        jblastPostAction.doCompleteAction(kJob,hista);            // workflow completed
+                        thisb.doCompleteAction(kJob,hista);            // workflow completed
                     },10);
                 }
             });
 
         },3000);
+    },
+
+    /**
+     * Read output of last generated file, copy results to /jblastdata, insert track to trackList.json.
+     * 
+     * @param {object} kJob - kue job object
+     * @param {object} hista - associative array of histories
+     * 
+     */
+    doCompleteAction(kJob,hista) {
+        var wId = kJob.data.workflow;
+        sails.log.debug(wId,'doCompleteAction()');
+        
+        var steps = kJob.data.workflowData.steps;
+        var g = sails.config.globals.jbrowse;
+        var dataSetPath = g.jbrowsePath + kJob.data.dataset + '/';    //g.dataSet[0].dataPath;
+        var targetDir = dataSetPath + g.jblast.blastResultPath;
+        
+        //sails.log("steps",steps);
+        kJob.data.blastData.outputs = {};
+        
+        var filecount = 0;  // this is used to identify number of outstanding files to copy, when it reaches zero, we are finished copying.
+        
+        //sails.log.debug("wf steps", steps);
+        
+        // find entries with "export" labels and copy those files to the dataset path
+        var filesToMove = 0;
+        for(var i in steps) {
+            //var label = steps[i].workflow_step_label;
+            var id = steps[i].job_id;
+            //sails.log.debug(wId,'step',i,"typeof label",label);
+            sails.log('step',i,'id',id);
+            //if (label != null && label.indexOf('export') !== -1) {
+            // check extension is importable (defined in the global import list)
+            if (id !== null && typeof hista[id].extension !== 'undefined' && g.jblast.import.indexOf(hista[id].extension) !== -1) {
+            
+                filesToMove++;
+                //sails.log('label',label);
+                /* istanbul ignore next */
+                if (typeof hista[id] === 'undefined'){
+                    sails.log.error("error undefined hist[id]",id);
+                    continue;
+                }
+                sails.log.debug('id, hista[id]',id,hista[id],hista[id].extension);
+                var ext = hista[id].extension;
+                var url = g.galaxy.galaxyUrl+'/'+hista[id].url + "/display";
+                var hid = hista[id].hid;
+                //var filename = hid+'_'+id;  //+'.'+ext;
+                var assetId = kJob.id+'_'+id;  //+'.'+ext;
+                var filepath = targetDir + '/' + assetId + '.'+ext;
+                
+                kJob.data.blastData.outputs[ext] = assetId; 
+                //kJob.data.blastData.assetId = assetId; 
+                
+                sails.log("id ")
+                sails.log.info(wId,'writing',filepath);
+                filecount++;
+                
+                // move the files to the destination folder
+                try {
+                    var stream = request(url).pipe(fs.createWriteStream(filepath));
+                    stream.on('finish', function () {     // detect file finished copying
+                        sails.log.debug("finished file");
+                        filecount--;
+                    });
+                }
+                catch(err) {
+                    /* istanbul ignore next */
+                    sails.log.error("Failed to write",filepath)
+                }
+            }
+        }
+        /* istanbul ignore if */
+        if (filesToMove==0) {
+            var msg = "No files to export.  Is the label: export [type] defined in the workflow?";
+            sails.log.error(msg);
+            kJob.kDoneFn(new Error(msg));
+        }
+        else {
+            // wait for files to finish copying
+            var t = setInterval(function() {
+                if (filecount === 0) {
+                    sails.log.debug("done moving files");
+                    //kJob.update(function() {});
+
+                        // insert track into trackList.json
+                        postMoveResultFiles(kJob,function(newTrackJson){
+
+                        // convert xml to json
+                        blast2json.convert(kJob,newTrackJson,function(err) {
+                            // istanbul ignore next
+                            if (err) {
+                                sails.log.error(err.msg);
+                                kJob.kDoneFn(new Error(err.msg));
+                                return;
+                            }
+                            sails.log.debug("post convert newTrackJson",newTrackJson);
+
+                            // check if there were any hits.
+                            // istanbul ignore if
+                            if (getHits(kJob,newTrackJson)===0) {
+
+                                kJob.data.name = kJob.data.name+' No Hits';
+                                kJob.update();
+                        
+                                var msg = "No Blast Hits";
+                                sails.log.error(msg);
+                                kJob.kDoneFn(new Error(msg));
+                            }
+                            else {
+                                offsetfix.process(kJob,newTrackJson,function() {
+                                    processFilter(kJob,newTrackJson,function(hitdata) {
+                                        // postAction is a service in JBConnect
+                                        postAction.addToTrackList(kJob,newTrackJson);
+                                    });
+                                });
+                            }
+
+                        });
+
+                    });
+                    clearInterval(t);
+                }
+            },100);
+        }
     }
+
 };
