9d9f2ef4475b098a317b313c7a3b6a9d4f989f81
enuggetry <ericiam@berkeley.edu>
2018-11-29 20:52:35 -0500

    add jbutil-ext.js for debugging.

--- test/jblast-dev.sh
+++ test/jblast-dev.sh
@@ -4,3 +4,4 @@ cp -av ../jblast-jbconnect-hook/test/jblast-int/* node_modules/jblast-jbconnect-
 cp -av ../jblast-jbconnect-hook/test/jblast/* node_modules/jblast-jbconnect-hook/test/jblast
 cp -av ../jblast-jbconnect-hook/test/jblast-dev.sh node_modules/jblast-jbconnect-hook/test/jblast-dev.sh
 cp -av ../jblast-jbconnect-hook/test/.nycrc-jblast node_modules/jblast-jbconnect-hook/test/.nycrc-jblast
+cp -av ../jblast-jbconnect-hook/bin/jbutil-ext.js node_modules/jblast-jbconnect-hook/bin/jbutil-ext.js
