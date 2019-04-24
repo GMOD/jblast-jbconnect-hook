9d849d44ec95706d252af10d9a5ffe5ba9f80fdd
enuggetry <ericiam@berkeley.edu>
2019-04-24 00:18:35 -0400

    Blast dialog sample sequence buttons - Fix #214

--- plugins/JBlast/js/queryDialog.js
+++ plugins/JBlast/js/queryDialog.js
@@ -52,8 +52,7 @@ _dialogContent: function () {
             wfStr += "<option value='"+workflows[i].id+"'>"+workflows[i].name+"</option>";
         }
     }
-    var content = this.content = {};
-    var container = dom.create('div', { className: 'search-dialog' } );
+    let container = dom.create('div', { className: 'search-dialog' } );
 
     let workflowCombo = dom.create('div', {
         id: 'workflow-div',
@@ -63,6 +62,31 @@ _dialogContent: function () {
             '<select id="workflow-combo" name="workflow">'+wfStr+'</select>'
 
     },container);
+
+    let cfg = this.browser.config;
+    
+    console.log("demo",cfg);
+    if (cfg.demo && cfg.demo.blastButtons && cfg.demo.blastButtons[0]) {
+        let blastButtons = dom.create('div', {
+            id: 'blastButtons',
+            style: {border: "1px solid lightgrey",padding: "5px"}
+        },container);
+
+        let btn = cfg.demo.blastButtons;
+
+        for(let i = 0;i < btn.length;i++) {
+            new dButton({
+                label: btn[i].button,
+                title: btn[i].description,
+                onClick: function() {
+                    console.log("click demo",btn[i].sequence.join());
+                    $('#sequence-text').val(btn[i].sequence.join());
+                }
+            })
+            .placeAt( blastButtons );
+        }
+    }
+
 
     // Render textarea box
     var searchBoxDiv = dom.create('div', {
